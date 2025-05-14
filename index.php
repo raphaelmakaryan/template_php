71daa1374b3ba0cff178b018fa397cf8f1e04350
MAKARYAN Raphael <contact@raphaelmakaryan.fr>
2025-05-14 17:39:04 +0200

    feat: enfin reussir le front controller

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
+$requested_page = isset($_GET['page']) ? $_GET['page'] : 'notfound';
 
-switch ($_SERVER['REQUEST_URI']) {
-    case '/page1':
-        include 'page1.php';
+switch ($requested_page) {
+    case "page1":
+        include(__DIR__ . "/page1.php");
         break;
-    case '/page2':
-        include 'page2.php';
+    case "page2":
+        include(__DIR__ . "/page2.php");
         break;
-    case '/page3':
-        include 'page3.php';
+    case "page3":
+        include(__DIR__ . "/page3.php");
+        break;
+    case "contact":
+        include(__DIR__ . "/contact.php");
         break;
     default:
-        include 'notfound.php';
+        include __DIR__ . "/notfound.php";
         break;
 }
