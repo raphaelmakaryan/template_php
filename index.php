ebe7a28d18b4d5aea8c8baa51d6f93f732e6ffca
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-05-14 15:00:43 +0200

    feat: premier ajout de l'itération 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include('./public/structure/header.php'); ?>
+
+<main>
+    <section class="mt-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <p class="fs-1">Voici ma page index.php</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include('./public/structure/footer.php'); ?>
---
+++ public/structure/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <p>© 2024 Mon Site Web. Tous droits réservés.</p>
+            </div>
+        </div>
+    </div>
+</foote>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+
+</html>
