b137aa1fcbda1cb677b88270a3f03a3c03f2edb1
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-05-19 12:30:05 +0200

    fix: ajout d'une fonction v2 de la calculatrice

--- public/iteration1/ex2/calculator.php
+++ public/iteration1/ex2/calculator.php
@@ -1,6 +1,6 @@
 <?php
 //var_dump($argv);
-function calcul($demande)
+function calculv1($demande)
 {
     if ($demande[1] === "+" || $demande[1] === "add") {
         $valeur1 = intval($demande[2]);
@@ -36,4 +36,34 @@ function calcul($demande)
     }
 }
 
-calcul($argv);
+//calculv1($argv);
+
+function calculv2($demande)
+{
+    switch ($demande) {
+        case $demande[1] === "+" || $demande[1] === "add":
+            echo intval($demande[2]) + intval($demande[3]);
+            break;
+        case $demande[1] === "-" || $demande[1] === "sub":
+            echo intval($demande[2]) - intval($demande[3]);
+            break;
+        case $demande[1] === "*" || $demande[1] === "mult":
+            echo intval($demande[2]) * intval($demande[3]);
+            break;
+        case $demande[1] === "/" || $demande[1] === "div":
+            $valeur1 = intval($demande[2]);
+            $valeur2 = intval($demande[3]);
+            if ($valeur1 == 0 || $valeur2 == 0) {
+                die("invalid division");
+            } else {
+                echo  $valeur1 /  $valeur2;
+            }
+            break;
+        case $demande[1] === "%" || $demande[1] === "mod":
+            echo intval($demande[2]) % intval($demande[3]);
+            break;
+        default:
+            die("unknown operation");
+    }
+}
+calculv2($argv);
